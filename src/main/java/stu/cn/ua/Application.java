2672db8e77e71ed5c194db61892a4d955e006894
panteliienko.danylo <panteliienko.danylo@gmail.com>
2017-04-30 07:11:04 +0300

    test

--- src/main/java/stu/cn/ua/Application.java
+++ src/main/java/stu/cn/ua/Application.java
@@ -2,12 +2,13 @@ package stu.cn.ua;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+       // SpringApplication.run(Application.class, args);
+        System.out.println("Hello World");
     }
 }
